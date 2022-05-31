832bea62dd5af2f70f651a18fad471ea18ec1a4e
microdotmatrix <liquid.knives.2@gmail.com>
2022-05-31 08:26:41 -0500

    commit

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 module.exports = {
   sanity: {
-    projectId: 'afxi85wm',
+    projectId: '7ljyzb3i',
     dataset: 'production',
     apiVersion: '2022-05-01',
     useCdn: true
   },
   envUrls: {
     development: 'http://localhost:3000',
-    staging: 'http://dev.bymattlee.com',
+    staging: 'http://11sane-kit.wtf/dist',
     production: 'https://bymattlee-11ty-starter.netlify.app'
   }
 }
